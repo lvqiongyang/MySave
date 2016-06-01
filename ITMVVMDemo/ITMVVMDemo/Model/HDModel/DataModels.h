c7db9c773a2ebcd5cbdf4292188bc6e14e8eccdc
lvqiongyang <lvqiongyang@163.com>
2016-06-01 10:03:18 +0800

    Revert "lvqiongyang"
    This reverts commit 704ac4517978aacb4a0336d3e4fa2c9d7fd25387.

--- ITMVVMDemo/ITMVVMDemo/Model/HDModel/DataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by Box  on 16/4/6
-//  Copyright (c) 2016 __MyCompanyName__. All rights reserved.
-//
-
-#import "HDIssues.h"#import "HDAuthor.h"#import "HDCursor.h"#import "HDContent.h"#import "HDHDBasicClass.h"#import "HDImage.h"#import "HDContents.h"#import "HDItems.h"
--- ITMVVMDemo/Pods/Target Support Files/GCJSONKit/GCJSONKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GCJSONKit : NSObject
-@end
-@implementation PodsDummy_GCJSONKit
-@end
